d4288d584274391c843efb7d929cc9db95047274
Student <student@richmw34567bel.rsd38.ca>
2018-01-09 18:39:49 -0800

    Initial Commit

---
+++ IDK/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IDK
+//
+//  Created by Student on 2018-01-09.
+//  Copyright © 2018 No Clue. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
