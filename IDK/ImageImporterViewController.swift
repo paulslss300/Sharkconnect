d5792bce60feb8516f4d02c8a682777e3c49dc97
launchpad <launchpad@bh-3d-02.rpl.richmond.bc.ca>
2018-02-11 14:11:34 -0800

    implemented gallery access

---
+++ IDK/ImageImporterViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ImageImporterViewController.swift
+//  IDK
+//
+//  Created by launchpad on 2018-02-11.
+//  Copyright © 2018 No Clue. All rights reserved.
+//
+
+import UIKit
+
+class ImageImporterViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func importImage(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        image.allowsEditing = false
+        
+        self.present(image, animated: true)
+        {
+            //After it is complete
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            myImageView.image = image
+        }else{
+            //Error message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
