ae5df5aa31a26dfc9f9fdd9d28313231a9e3ce87
Student <student@richmw34567bel.rsd38.ca>
2018-02-13 19:49:35 -0800

    collection view

---
+++ DiscoverCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DiscoverCollectionViewCell.swift
+//  IDK
+//
+//  Created by Student on 2018-02-13.
+//  Copyright © 2018 No Clue. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var clubImage: UIImageView!
+    @IBOutlet weak var clubName: UILabel!
+    
+}
---
+++ DiscoverViewController.swift
@@ -0,0 +1,60 @@
+//
+//  DiscoverViewController.swift
+//  IDK
+//
+//  Created by Student on 2018-02-13.
+//  Copyright © 2018 No Clue. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate  {
+    @IBOutlet weak var discoverView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let itemSize = UIScreen.main.bounds.width/2 
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsetsMake(20, 0, 10, 0)
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        layout.minimumInteritemSpacing = 3
+        layout.minimumLineSpacing = 3
+        
+        discoverView.collectionViewLayout = layout
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Club.clubs.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "discovercells", for: indexPath) as! DiscoverCollectionViewCell
+        let club = Club.clubs[indexPath.row]
+        
+        cell.clubName?.text = club.ClubNa
+        cell.clubImage.image = club.ClubCellImageName
+        
+        return cell
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
