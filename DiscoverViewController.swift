700d4930000abab7fe285daa68d3ded03ad6a682
Paul Tang <paultang@Pauls-Air.hitronhub.home>
2018-05-18 12:02:59 -0700

    searchbar finished, improve the search ability in the future

--- DiscoverViewController.swift
+++ DiscoverViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-class DiscoverViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class DiscoverViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     @IBOutlet weak var discoverView: UICollectionView!
     
+    var isSerching: Bool = false
+    
     var selectedClub: Club? = nil
+    
+    var filteredClubs = [Club]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         discoverView.delegate  = self
         discoverView.dataSource = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
         
         let itemSize = UIScreen.main.bounds.width/2 - 50
         let layout = UICollectionViewFlowLayout()
@@ -39,23 +47,57 @@ class DiscoverViewController: UIViewController, UICollectionViewDataSource, UICo
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
         let club = Club.clubs[indexPath.row]
+        
+        if isSerching {
+            let club = filteredClubs[indexPath.row]
+        }
+        
         selectedClub = club
         performSegue(withIdentifier: "showclubinformation", sender: self)  
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        if isSerching {
+            return filteredClubs.count
+        }
+        
         return Club.clubs.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "discovercells", for: indexPath) as! DiscoverCollectionViewCell
-        let club = Club.clubs[indexPath.row]
+        
+        var club = Club.clubs[indexPath.row]
+        
+        if isSerching {
+            club = filteredClubs[indexPath.row]
+        }
+        
         cell.clubName?.text = club.ClubNa
         cell.clubImage.image = club.ClubCellImageName
         
         return cell
         
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            isSerching = false
+            view.endEditing(true)
+            discoverView.reloadData()
+        } else {
+            isSerching = true
+            filteredClubs = Club.clubs.filter({$0.ClubNa == searchBar.text!})
+            /*
+            filteredClubs = Club.clubs.filter({ (club) -> Bool in
+                return !(club.ClubNa == searchBar.text!)
+            })
+             */
+            discoverView.reloadData()
+        }
     }
     
 
